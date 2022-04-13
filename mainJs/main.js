b884b04eef71f9ee7f871eaa3b67895f7ba5dea4
GEAN PEREIRA DE SOUZA <21276338@CFP127ALUNO.LOCAL>
2022-04-13 13:50:01 -0300

    color

--- mainJs/main.js
+++ mainJs/main.js
@@ -1,5 +1,12 @@
 'use strict'
 
+
+// export const getPokemonurl = (type) =>{
+//     return`
+//     <p class="card-subtitle" tipy-tag->${types}</p>
+//     `
+// }
+
 const pesquisarPoke = () =>{
 
     const getPokemonurl = id => `https://pokeapi.co/api/v2/pokemon/${id}`
@@ -14,15 +21,17 @@ const pesquisarPoke = () =>{
         const lisPokemons = pokemons.reduce((accumulator ,pokemon) => {
 
             const types = pokemon.types.map(typeInfo => typeInfo.type.name)
+            const typeTags = types.map((type) => renderTypeTag(type));
+
             accumulator += 
             `<div class="card">
                 <div class ="img">
                 <img class ="card-image ${types[0]}" alt="${pokemon.name}" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png" />
                 </div>
                 <div class ="atributos">
-                <p class="card-title">${pokemon.id}</p>
+                <p class="card-title">${pokemon.id}º</p>
                 <p class="card-name">${pokemon.name}</p>
-                <p class="card-subtitle">${types.join(' | ')}</p>
+                <p class="card-subtitle">${typeTags.join(' | ')}</p>
                 </div>
                 
             </div>`
@@ -32,6 +41,12 @@ const pesquisarPoke = () =>{
        
         ul.innerHTML = lisPokemons
     })
+}
+
+const renderTypeTag = (type) => {
+    return `
+        <span class="type-tag type-tag-${type}">${type}</span>
+    `;
 }
 
 pesquisarPoke()
